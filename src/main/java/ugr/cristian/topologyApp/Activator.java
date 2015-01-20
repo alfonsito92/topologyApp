3b4ed0f02f9bcb16630ac77db37e646ae4fe0a11
alfonsito92 <cristian.pr.sa@gmail.com>
2015-01-20 16:24:34 +0100

    Estructura de la interfaz que reacciona a los cambios en la topologia

--- src/main/java/ugr/cristian/topologyApp/Activator.java
+++ src/main/java/ugr/cristian/topologyApp/Activator.java
@@ -25,38 +25,33 @@ import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
 import org.opendaylight.controller.sal.flowprogrammer.IFlowProgrammerService;
 import org.opendaylight.controller.sal.packet.IDataPacketService;
 import org.opendaylight.controller.sal.packet.IListenDataPacket;
+import org.opendaylight.controller.sal.topology.IListenTopoUpdates;
 import org.opendaylight.controller.switchmanager.ISwitchManager;
 import org.opendaylight.controller.topologymanager.ITopologyManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Activator extends ComponentActivatorAbstractBase {
-    private static final Logger log = LoggerFactory.getLogger(PacketHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(TopoHandler.class);
 
     public Object[] getImplementations() {
         log.trace("Getting Implementations");
 
-        Object[] res = { PacketHandler.class };
+        Object[] res = { TopoHandler.class };
         return res;
     }
 
     public void configureInstance(Component c, Object imp, String containerName) {
         log.trace("Configuring instance");
 
-        if (imp.equals(PacketHandler.class)) {
-            // Define exported and used services for PacketHandler component.
+        if (imp.equals(TopoHandler.class)) {
+            // Define exported and used services for TopoHandler component.
 
             Dictionary<String, Object> props = new Hashtable<String, Object>();
-            props.put("salListenerName", "mypackethandler");
+            props.put("salListenerName", "mytopohandler");
 
-            // Export IListenDataPacket interface to receive packet-in events.
-            c.setInterface(new String[] {IListenDataPacket.class.getName()}, props);
-
-            // Need the DataPacketService for encoding, decoding, sending data packets
-            c.add(createContainerServiceDependency(containerName).setService(
-                    IDataPacketService.class).setCallbacks(
-                    "setDataPacketService", "unsetDataPacketService")
-                    .setRequired(true));
+            // Export IListenTopoUpdates interface to receive packet-in events.
+            c.setInterface(new String[] {IListenTopoUpdates.class.getName()}, props);
 
             // Need FlowProgrammerService for programming flows
             c.add(createContainerServiceDependency(containerName).setService(
---
+++ src/main/java/ugr/cristian/topologyApp/TopoHandler.java
@@ -0,0 +1,170 @@
+/**
+Copyright (C) 2015  Cristian Alfonso Prieto Sánchez
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ugr.cristian.topologyApp;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.opendaylight.controller.sal.action.Action;
+import org.opendaylight.controller.sal.action.Output;
+import org.opendaylight.controller.sal.action.SetDlDst;
+import org.opendaylight.controller.sal.action.SetDlSrc;
+import org.opendaylight.controller.sal.action.SetNwDst;
+import org.opendaylight.controller.sal.action.SetNwSrc;
+import org.opendaylight.controller.sal.core.ConstructionException;
+import org.opendaylight.controller.sal.core.Node;
+import org.opendaylight.controller.sal.core.NodeConnector;
+import org.opendaylight.controller.sal.flowprogrammer.Flow;
+import org.opendaylight.controller.sal.flowprogrammer.IFlowProgrammerService;
+import org.opendaylight.controller.sal.match.Match;
+import org.opendaylight.controller.sal.match.MatchType;
+import org.opendaylight.controller.sal.packet.Ethernet;
+import org.opendaylight.controller.sal.packet.IDataPacketService;
+import org.opendaylight.controller.sal.packet.IListenDataPacket;
+import org.opendaylight.controller.sal.packet.IPv4;
+import org.opendaylight.controller.sal.packet.Packet;
+import org.opendaylight.controller.sal.packet.PacketResult;
+import org.opendaylight.controller.sal.packet.RawPacket;
+import org.opendaylight.controller.sal.packet.TCP;
+import org.opendaylight.controller.sal.utils.EtherTypes;
+import org.opendaylight.controller.sal.utils.Status;
+import org.opendaylight.controller.sal.topology.*;
+import org.opendaylight.controller.sal.core.Edge;
+import org.opendaylight.controller.switchmanager.ISwitchManager;
+import org.opendaylight.controller.topologymanager.ITopologyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TopoHandler implements IListenTopoUpdates {
+
+    private static final Logger log = LoggerFactory.getLogger(TopoHandler.class);
+
+    private IFlowProgrammerService flowProgrammerService;
+    private ISwitchManager switchManager;
+    private ITopologyManager topologyManager;
+
+    static private InetAddress intToInetAddress(int i) {
+        byte b[] = new byte[] { (byte) ((i>>24)&0xff), (byte) ((i>>16)&0xff), (byte) ((i>>8)&0xff), (byte) (i&0xff) };
+        InetAddress addr;
+        try {
+            addr = InetAddress.getByAddress(b);
+        } catch (UnknownHostException e) {
+            return null;
+        }
+
+        return addr;
+    }
+
+    public TopoHandler() {
+        /**try {
+            publicInetAddress = InetAddress.getByName(PUBLIC_IP);
+        } catch (UnknownHostException e) {
+            log.error(e.getMessage());
+        }
+
+        try {
+            server1Address = InetAddress.getByName(SERVER1_IP);
+        } catch (UnknownHostException e) {
+            log.error(e.getMessage());
+        }
+
+        try {
+            server2Address = InetAddress.getByName(SERVER2_IP);
+        } catch (UnknownHostException e) {
+            log.error(e.getMessage());
+        }*/
+    }
+
+
+    /**
+     * Sets a reference to the requested FlowProgrammerService
+     */
+    void setFlowProgrammerService(IFlowProgrammerService s) {
+        log.trace("Set FlowProgrammerService.");
+
+        flowProgrammerService = s;
+    }
+
+    /**
+     * Unsets FlowProgrammerService
+     */
+    void unsetFlowProgrammerService(IFlowProgrammerService s) {
+        log.trace("Removed FlowProgrammerService.");
+
+        if (flowProgrammerService == s) {
+            flowProgrammerService = null;
+        }
+    }
+
+    /**
+     * Sets a reference to the requested SwitchManagerService
+     */
+    void setSwitchManagerService(ISwitchManager s) {
+        log.trace("Set SwitchManagerService.");
+
+        switchManager = s;
+    }
+
+    /**
+     * Unsets SwitchManagerService
+     */
+    void unsetSwitchManagerService(ISwitchManager s) {
+        log.trace("Removed SwitchManagerService.");
+
+        if (switchManager == s) {
+            switchManager = null;
+        }
+    }
+
+    /**
+    * Sets a reference to the requested TopologyManagerService
+    */
+    void setTopologyManagerService(ITopologyManager s) {
+      log.trace("Set TopologyManagerService.");
+
+      topologyManager = s;
+    }
+
+    /**
+    * Unsets SwitchManagerService
+    */
+    void unsetTopologyManagerService(ITopologyManager s) {
+      log.trace("Removed TologyManagerService.");
+
+      if (topologyManager == s) {
+        topologyManager = null;
+      }
+    }
+
+
+    @Override
+    public void edgeUpdate(List<TopoEdgeUpdate> topoedgeupdateList) {
+
+        log.trace("Pasa algo");
+
+    }
+
+    @Override
+    public void edgeOverUtilized(Edge edge){}
+
+    @Override
+    public void edgeUtilBackToNormal(Edge edge){}
+
+}
